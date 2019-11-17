099f306b3c22f62e65eb7fdf623fa9431467e517
avnicolaevich <avnicolaevich@gmail.com>
2019-11-17 10:26:58 +0200

    service: refactor request function for better understanding

--- src/service.js
+++ src/service.js
@@ -1,8 +1,16 @@
-const BASE_URI = 'https://api.unsplash.com';
-
 export default class GalleryService {
-    getResponseApi = async(request='') => {
-        const response = await fetch(`${BASE_URI}${request}`, {
+    getPhotos = async(page) => {
+        const response = await fetch("https://api.unsplash.com/photos?page="+page+"&per_page=20", {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Client-ID cff11d975e766b0d4ed0f6b3ebb2647b75523b96c4d321820c3851401c54ba9b',
+                'Accept-Version': 'v1'
+            }
+        });
+        return await response.json();
+    };
+    getRandomPhoto = async() => {
+        const response = await fetch("https://api.unsplash.com/photos/random", {
             method: 'GET',
             headers: {
                 'Authorization': 'Client-ID cff11d975e766b0d4ed0f6b3ebb2647b75523b96c4d321820c3851401c54ba9b',
